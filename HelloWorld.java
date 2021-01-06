34c4f9b9ada074316d928dec20932a631ada2de6
Vanessa Beeler <vbeeler@calpoly.edu>
2021-01-06 11:19:09 -0800

    change by Vanessa Beeler

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my third year at calpoly. ");
+        System.out.println("Change by Vanessa Beeler");
     }
 }
